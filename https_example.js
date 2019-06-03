274654599e530d5b89cbc124fd0c2c123d662e59
S-Marks <marks.cherie.art@gmail.com>
2019-06-03 00:43:49 +0000

    response

--- https_example.js
+++ https_example.js
@@ -1,4 +1,4 @@
-var http = require("http");
+var https = require("https");
 
 var options = {
   host: 'www.example.org',
@@ -6,12 +6,17 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+  console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+  console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
 
-http.request(options, callback).end();
+https.request(options, callback).end();
 
 console.log("I've made the request!");
